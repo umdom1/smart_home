087d36e9b920e925ce27e2ad0b6550b472cc8c02
Denis Denisov <umdom1@gmail.com>
2023-11-06 23:07:35 +0300

    Second commit

--- measurement/models.py
+++ measurement/models.py
@@ -7,7 +7,7 @@ class Sensor(models.Model):
 
 
 class Measurement(models.Model):
-    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name="measurements")
     temperature = models.DecimalField(max_digits=4, decimal_places=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
